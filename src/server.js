82c69cb9dcf3c44142c608ba3bba166856a0434a
Mark McCarthy <mm3guides@gmail.com>
2024-08-07 08:21:16 +0100

    Connection string modified to point to M56 db and try catch added

--- src/server.js
+++ src/server.js
@@ -9,22 +9,30 @@ const app = express();
 app.use(express.json());
 
 app.post("/books/addbook", async (req, res) => {
-
+try {
     const result = await Book.create({
         title: req.body.title,
         author: req.body.author,
         genre: req.body.genre
     });
 
-    console.log(result);
-
     const responseMessage = {
-        message: `Book ${req.body.title}added`,
+        message: `Book ${req.body.title} added`,
         dbresponse: result
-    }
-
+    };
 
     res.status(201).send(responseMessage);
+
+} catch (error) {
+    
+    const responseMessage = {
+        message: `Book ${req.body.title} NOT added`,
+        dbresponse: error
+    };
+
+    res.status(400).send(responseMessage);
+}
+
 });
 
 app.listen(5001, () => {console.log("Server is listening on port 5001")});
